9ce28069374700073bed957ab677c77903a0124a
taiseerrashed <taiseerrashed@gmail.com>
2024-03-27 20:22:58 +0200

    commit2

--- src/components/StoreItem.js
+++ src/components/StoreItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button, Card } from "react-bootstrap";
 import formatCurrency from "./FormatCurrency";
 import { useShoppingCart } from "../context/ShoppingCartContext";
